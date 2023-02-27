e1fb819086f25f528cd85e7aad8448df9fe33edd
smolyak <smolyak@svyazcom.ru>
2023-02-27 22:39:35 +0700

    player with SMOOTH camera

--- src/main.rs
+++ src/main.rs
@@ -14,46 +14,12 @@ fn main()
             ..Default::default()
         }))
         .add_plugin(WorldInspectorPlugin)
-        .add_startup_system(startup_system)
-        .add_system(camera_system)
+        .add_plugin(myaso::player::PlayerPlugin)
+        .add_startup_system(startup)
         .run();
 }
 
-fn startup_system(mut commands: Commands, asset_server: Res<AssetServer>)
+fn startup(mut commands: Commands)
 {
     commands.spawn(Camera2dBundle::default());
-
-    commands.spawn((SpriteBundle {
-        texture: asset_server.load("bro.png"),
-        ..Default::default()
-    })); // here is PLAYER
 }
-
-
-
-
-fn camera_system(
-    keys: Res<Input<KeyCode>>,
-    mut camera_query: Query<(&mut Transform, With<Camera>)>
-)
-{
-    let mut transform = camera_query.single_mut().0;
-    let mut vector: Vec3 = Vec3::default();
-
-
-    if keys.pressed(KeyCode::W) {
-        vector = vector + Vec3{y: 1.0, ..Default::default()};
-    }
-    if keys.pressed(KeyCode::A) {
-        vector = vector + Vec3{x: -1.0, ..Default::default()};
-    }
-    if keys.pressed(KeyCode::S) {
-        vector = vector + Vec3{y: -1.0, ..Default::default()};
-    }
-    if keys.pressed(KeyCode::D) {
-        vector = vector + Vec3{x: 1.0, ..Default::default()};
-    }
-    
-    transform.translation = transform.translation + vector;
-}
-
--- src/myaso/player.rs
+++ src/myaso/player.rs
@@ -2,7 +2,8 @@ use bevy::prelude::*;
 
 const LOCAL_ID: u32 = 0;
 const LOCAL_NAME: &str = "player_local";
-
+const PLAYER_SPEED: f32 = 100.0;
+const SMOOTH_CAMERA_SPEED: f32 = 0.1;
 // component to indicate Player with ID
 #[derive(Component)]
 pub struct Player
@@ -22,7 +23,8 @@ impl Plugin for PlayerPlugin
         app
             // spawning player
             .add_startup_system(spawn_player)
-            .add_system(player_move);
+            .add_system(player_move)
+            .add_system(player_camera_follow);
     }
 }
 
@@ -42,20 +44,70 @@ impl Default for PlayerBundle {
     }
 }
 
-fn spawn_player(mut commands: Commands)
+fn spawn_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>
+)
 {
     // constucting own player in single -- always id == 0
     commands.spawn(
-        PlayerBundle {
+    // some player info
+    (PlayerBundle {
             ..Default::default()
-        }
-    );
+        },
+    // drawing
+    SpriteBundle {
+        texture: asset_server.load("bro.png"),
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(100.0, 100.0)),
+            ..Default::default()
+        },
+        ..Default::default()
+    }
+    ));
 }
 
 fn player_move(
     keys: Res<Input<KeyCode>>,
-    mut query: Query<&mut Player>
+    mut query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>
 )
 {
+    let mut transform = query.single_mut();
+    let mut vector: Vec3 = Vec3::default();
 
+    let sprint_mult = if keys.pressed(KeyCode::LShift) {2.0} else {1.0};
+    let mult = sprint_mult * time.delta_seconds();
+
+
+    if keys.pressed(KeyCode::W) {
+        vector = vector + Vec3{y: PLAYER_SPEED * mult, ..Default::default()};
+    }
+    if keys.pressed(KeyCode::A) {
+        vector = vector + Vec3{x: -PLAYER_SPEED * mult, ..Default::default()};
+    }
+    if keys.pressed(KeyCode::S) {
+        vector = vector + Vec3{y: -PLAYER_SPEED * mult, ..Default::default()};
+    }
+    if keys.pressed(KeyCode::D) {
+        vector = vector + Vec3{x: PLAYER_SPEED * mult, ..Default::default()};
+    }
+    
+    transform.translation = transform.translation + vector;
+}
+
+fn player_camera_follow(
+    mut set: ParamSet<(Query<&mut Transform, With<Player>>, Query<&mut Transform, With<Camera>>)>
+)
+{
+    let mut p_query: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+    for player in set.p0().iter()
+    {
+        p_query = player.translation;
+    }
+    for mut cam_pos in set.p1().iter_mut()
+    {
+        cam_pos.translation = cam_pos.translation.lerp(p_query, SMOOTH_CAMERA_SPEED);
+    }
+    
 }
